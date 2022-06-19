e3fdda22df0ae5d20e1ed8a8b439dc61ce4a8d80
Daniel Paulo <danielpaulo199@gmail.com>
2022-06-19 15:15:07 -0300

    Adicionado sprites do ninho e logica para mudar eles

--- Assets/Scripts/ShowUI.cs
+++ Assets/Scripts/ShowUI.cs
@@ -6,6 +6,14 @@ public class ShowUI : MonoBehaviour
 {
     public GameObject uiObject;
     
+    public Sprite nest2Eggs;
+    public Sprite nest1Eggs;
+    public Sprite nest0Eggs;
+    public SpriteRenderer render;
+
+    public int eggs = 3;
+
+    
     private void Start()
     {
         uiObject.SetActive(false);
@@ -17,6 +25,20 @@ public class ShowUI : MonoBehaviour
         {
             uiObject.SetActive(true);
             StartCoroutine("ShowTime");
+            eggs = --eggs;
+            
+            if (eggs == 2){
+                render.sprite = nest2Eggs;
+            }
+            
+             if (eggs == 1){
+                render.sprite = nest1Eggs;
+            }
+            
+             if (eggs == 0){
+                render.sprite = nest0Eggs;
+                Debug.Log("Game over");
+            }
         }
     }
 
